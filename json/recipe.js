9829039603a5df52bc227057f4902fb786e7472a
Sylvia Korshie-Amemo <sekorshie-amemo@st.ug.edu.gh>
2023-07-24 05:31:19 -0700

    Oblayo ID updated

--- json/recipe.js
+++ json/recipe.js
@@ -52,8 +52,8 @@ const recipes = [
         img: require("../assets/oblayo_breakfast.jpg"),
         name: "Oblayo",
         category: "breakfast",
-        ingredients: "\u2022 Roughly ground corn\n\u2022 5 1/2 -6 cups water\n\u2022 1 teaspoon salt\n\u2022 1/2 teaspoon nutmeg\n\u2022 honey/sugar any sweetener of choice\n\u2022 powdered or evaporated milk to serve\n\u2022 Dash of nutmeg for garnish optional",
-        instructions: "1.Pour some corn in a bowl and add water to it.\n2. Boil for about 20 minutes till the rice has soaked the water and its soft. Add the salt and the other one cup water if needed. Using a ladle, beat the rice against the saucepan to form a smooth pudding of consistency.\n3. Add the nutmeg and sweetner of choice let it simmer for about five minutes.\n4. Serve with milk of choice and sprinkle with tiny dash of nutmeg for garnish."
+        ingredients: "\u2022 Roughly ground corn\n\u2022 Water\n\u2022 1 teaspoon salt\n\u2022 1/2 teaspoon nutmeg\n\u2022 honey/sugar any sweetener of choice\n\u2022 powdered or evaporated milk to serve\n\u2022 Dash of nutmeg for garnish optional",
+        instructions: "1.Pour some corn in a bowl and add water to it.\n2.Stir and wait for the corn to settle, the chaff will surface. Add the salt and the other one cup water if needed. Using a ladle, beat the rice against the saucepan to form a smooth pudding of consistency.\n3. Add the nutmeg and sweetner of choice let it simmer for about five minutes.\n4. Serve with milk of choice and sprinkle with tiny dash of nutmeg for garnish."
         
     }
 ]
