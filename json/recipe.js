cd6503749ee0094ed8368c1e021d1f0bbb267239
Sylvia Korshie-Amemo <sekorshie-amemo@st.ug.edu.gh>
2023-07-24 06:09:47 -0700

    Ingredient update to breakfast

--- json/recipe.js
+++ json/recipe.js
@@ -52,8 +52,8 @@ const recipes = [
         img: require("../assets/oblayo_breakfast.jpg"),
         name: "Oblayo",
         category: "breakfast",
-        ingredients: "\u2022 Roughly ground corn\n\u2022 Water\n\u2022 1 teaspoon salt\n\u2022 1/2 teaspoon nutmeg\n\u2022 honey/sugar any sweetener of choice\n\u2022 powdered or evaporated milk to serve\n\u2022 Dash of nutmeg for garnish optional",
-        instructions: "1.Pour some corn in a bowl and add water to it.\n2.Stir and wait for the corn to settle, the chaff will surface. Add the salt and the other one cup water if needed. Using a ladle, beat the rice against the saucepan to form a smooth pudding of consistency.\n3. Add the nutmeg and sweetner of choice let it simmer for about five minutes.\n4. Serve with milk of choice and sprinkle with tiny dash of nutmeg for garnish."
+        ingredients: "\u2022 Roughly ground corn\n\u2022 Water\n\u2022 Sugar\n\u2022 1/2 teaspoon nutmeg\n\u2022 honey/sugar any sweetener of choice\n\u2022 powdered or evaporated milk to serve\n\u2022 Dash of nutmeg for garnish optional",
+        instructions: "1.Pour some corn in a bowl and add water to it.\n2.Stir and wait for the corn to settle, the chaff will surface, then pour away the chaff from it.\n3. Put water on fire in the saucepan, pour in your corn and stir from time to time and leave it to cook for a while.\n4. Cook until you can squash the corn.\n5.If you want it lighter, you can add more water, stir and let it cook for a little more time.\n 6.Pour it into a bowl and its ready. add your sugar and milk as you want. ."
         
     }
 ]
