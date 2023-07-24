ba4089144c054c1f35915046f5a1a4801082062f
Sylvia Korshie-Amemo <sekorshie-amemo@st.ug.edu.gh>
2023-07-24 04:54:07 -0700

    New object, oblayo added

--- json/recipe.js
+++ json/recipe.js
@@ -45,7 +45,16 @@ const recipes = [
         name: "Waakye",
         category: "supper",
         ingredients: "",
-        instructions: ""
+        instructions: "",
+    },
+    {
+        id: 7,
+        img: require("../assets/oblayo_breakfast.jpg"),
+        name: "Oblayo",
+        category: "breakfast",
+        ingredients: "\u2022 Roughly ground corn\n\u2022 5 1/2 -6 cups water\n\u2022 1 teaspoon salt\n\u2022 1/2 teaspoon nutmeg\n\u2022 honey/sugar any sweetener of choice\n\u2022 powdered or evaporated milk to serve\n\u2022 Dash of nutmeg for garnish optional",
+        instructions: "1.Pour some corn in a bowl and add water to it.\n2. Boil for about 20 minutes till the rice has soaked the water and its soft. Add the salt and the other one cup water if needed. Using a ladle, beat the rice against the saucepan to form a smooth pudding of consistency.\n3. Add the nutmeg and sweetner of choice let it simmer for about five minutes.\n4. Serve with milk of choice and sprinkle with tiny dash of nutmeg for garnish."
+        
     }
 ]
 
